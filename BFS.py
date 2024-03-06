5aa866847d027700418f0e90d74db37af1fc3bb8
Nandu-Ramineni <nandurameneni5577@gmail.com>
2024-03-06 20:31:34 +0530

    First commit

---
+++ BFS.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex, end=' ')
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+
+# Example usage:
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+start_vertex = 'A'
+print("BFS traversal:")
+bfs(graph, start_vertex)
