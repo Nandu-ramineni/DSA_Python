ffb18a1edb1fc5e7514c27acd305e28f1008e1c5
Nandu-Ramineni <nandurameneni5577@gmail.com>
2024-03-08 21:54:31 +0530

    First commit

--- BFS.py
+++ BFS.py
@@ -1,26 +1,53 @@
 from collections import deque
 
-def bfs(graph, start):
-    visited = set()
-    queue = deque([start])
+class Graph:
+    def __init__(self):
+        self.graph = {}
 
-    while queue:
-        vertex = queue.popleft()
-        if vertex not in visited:
-            print(vertex, end=' ')
-            visited.add(vertex)
-            queue.extend(graph[vertex] - visited)
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+        visited.add(start)
+
+        while queue:
+            node = queue.popleft()
+            print(node, end=" ")
+
+            if node in self.graph:
+                for neighbor in self.graph[node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+
+    def delete_edge(self, u, v):
+        if u in self.graph and v in self.graph[u]:
+            self.graph[u].remove(v)
+
+    def update_edge(self, u, v):
+        if u in self.graph:
+            self.graph[u].append(v)
 
 # Example usage:
-graph = {
-    'A': {'B', 'C'},
-    'B': {'A', 'D', 'E'},
-    'C': {'A', 'F'},
-    'D': {'B'},
-    'E': {'B', 'F'},
-    'F': {'C', 'E'}
-}
+g = Graph()
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 4)
+g.add_edge(2, 5)
+g.add_edge(3, 6)
 
-start_vertex = 'A'
 print("BFS traversal:")
-bfs(graph, start_vertex)
+g.bfs(1)
+
+# Delete an edge
+g.delete_edge(2, 4)
+
+# Update an edge
+g.update_edge(3, 7)
+
+print("\nUpdated BFS traversal:")
+g.bfs(1)
